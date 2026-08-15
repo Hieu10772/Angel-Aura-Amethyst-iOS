6e27b6d3747201c755e6e2dfab5f3f86b79f59b4
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-15 12:13:05 +0700

    Update CallbackBridge.java

--- JavaApp/src/lwjgl/org/lwjgl/glfw/CallbackBridge.java
+++ JavaApp/src/lwjgl/org/lwjgl/glfw/CallbackBridge.java
@@ -1,10 +1,12 @@
 package org.lwjgl.glfw;
-import java.util.*;
+
+import java.nio.ByteBuffer;
 
 public class CallbackBridge {
+
     public static final int CLIPBOARD_COPY = 2000;
     public static final int CLIPBOARD_PASTE = 2001;
-    
+
     public static final int EVENT_TYPE_CHAR = 1000;
     public static final int EVENT_TYPE_CHAR_MODS = 1001;
     public static final int EVENT_TYPE_CURSOR_ENTER = 1002;
@@ -14,32 +16,51 @@ public class CallbackBridge {
     public static final int EVENT_TYPE_MOUSE_BUTTON = 1006;
     public static final int EVENT_TYPE_SCROLL = 1007;
     public static final int EVENT_TYPE_WINDOW_SIZE = 1008;
-    
-    public static final int ANDROID_TYPE_GRAB_STATE = 0;
-    
-    public static final boolean INPUT_DEBUG_ENABLED;
-    
-    // TODO send grab state event to Android
-    
-    static {
-        INPUT_DEBUG_ENABLED = Boolean.parseBoolean(System.getProperty("glfwstub.debugInput", "false"));
 
-        
-/*
-        if (isDebugEnabled) {
-            //try {
-                //debugEventStream = new PrintStream(new File(System.getProperty("user.dir"), "glfwstub_inputeventlog.txt"));
-		    debugEventStream = System.out;
-            //} catch (FileNotFoundException e) {
-            //    e.printStackTrace();
-            //}
-        }
-	
-	    //Quick and dirty: debul all key inputs to System.out
-*/
+    public static final int ANDROID_TYPE_GRAB_STATE = 0;
+
+    public static final boolean INPUT_DEBUG_ENABLED;
+
+    public static boolean sGamepadDirectEnabled;
+
+    static {
+        INPUT_DEBUG_ENABLED = Boolean.parseBoolean(
+            System.getProperty("glfwstub.debugInput", "false")
+        );
     }
 
-    public static native String nativeClipboard(int action, byte[] copy);
+    public static void enableGamepadDirectInput() {
+        if (sGamepadDirectEnabled) {
+            return;
+        }
+
+        sGamepadDirectEnabled = nativeEnableGamepadDirectInput();
+    }
+
+    public static void sendData(int type, String data) {
+        nativeSendData(false, type, data);
+    }
+
+    public static native void nativeSendData(
+        boolean isAndroid,
+        int type,
+        String data
+    );
+
+    public static native boolean nativeSetInputReady(boolean ready);
+
+    public static native String nativeClipboard(
+        int action,
+        byte[] copy
+    );
+
     public static native void nativeSetGrabbing(boolean grab);
-	public static native void nativeSetCursorShape(int shape);
+
+    public static native void nativeSetCursorShape(int shape);
+
+    public static native ByteBuffer nativeCreateGamepadButtonBuffer();
+
+    public static native ByteBuffer nativeCreateGamepadAxisBuffer();
+
+    private static native boolean nativeEnableGamepadDirectInput();
 }
