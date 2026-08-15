855a2219852681c8b9766e45f08e901f780643b9
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-15 12:00:50 +0700

    Update CallbackBridge.java

--- lwgjl/lwjgl3-wip-rebase_3.4.1/modules/lwjgl/glfw/src/main/java/org/lwjgl/glfw/CallbackBridge.java
+++ lwgjl/lwjgl3-wip-rebase_3.4.1/modules/lwjgl/glfw/src/main/java/org/lwjgl/glfw/CallbackBridge.java
@@ -5,5 +5,5 @@ public final class CallbackBridge {
     private CallbackBridge() {
     }
 
-    public static native void nativeSetCursorShape(long window, int shape);
+    public static native void nativeSetCursorShape(int shape);
 }
