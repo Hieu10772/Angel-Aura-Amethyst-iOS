70d9f7078f6e7677e8d45ffbcf21c1f063dc2547
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-15 12:33:35 +0700

    Update CallbackBridge.java

--- lwgjl/lwjgl3-wip-rebase_3.4.1/modules/lwjgl/glfw/src/main/java/org/lwjgl/glfw/CallbackBridge.java
+++ lwgjl/lwjgl3-wip-rebase_3.4.1/modules/lwjgl/glfw/src/main/java/org/lwjgl/glfw/CallbackBridge.java
@@ -2,8 +2,15 @@ package org.lwjgl.glfw;
 
 public final class CallbackBridge {
 
+    public static final int CLIPBOARD_COPY = 2000;
+    public static final int CLIPBOARD_PASTE = 2001;
+
     private CallbackBridge() {
     }
 
     public static native void nativeSetCursorShape(int shape);
+
+    public static native void nativeSetGrabbing(boolean grab);
+
+    public static native String nativeClipboard(int action, byte[] copy);
 }
